8467493bc6486379e6e5757d819a65d19a6589ee
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-16 20:51:19 -0300

    request params

--- src/server.js
+++ src/server.js
@@ -4,4 +4,9 @@ const app = express()
 
 const PORT = 3333
 
+app.get("/message/:id", (request, response) => {
+    const {id} = request.params
+    response.send(`O id é de: ${id}`)
+})
+
 app.listen(PORT, () => {console.log(`Server is running on Port: ${PORT}`)})
