c33523ef7c309f46622fc91fe6baafef40f9ac9d
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-20 11:49:51 -0300

    added request query and nodemon

--- src/server.js
+++ src/server.js
@@ -9,4 +9,9 @@ app.get("/message/:id", (request, response) => {
     response.send(`O id é de: ${id}`)
 })
 
+app.get("/user", (request, response) => {
+    const {page, limit} = request.query
+    response.send(`A página é: ${page}.\n O limite atual é de: ${limit}`)
+})
+
 app.listen(PORT, () => {console.log(`Server is running on Port: ${PORT}`)})
