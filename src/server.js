59548eece770199f5a6d0be2cd1252fba782c894
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-20 12:17:33 -0300

    changing structure and using insomnia to post

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 const express = require('express')
 
+const routes = require("./routes")
+
 const app = express()
 
+app.use(express.json())
+
+app.use(routes)
+
+
 const PORT = 3333
-
-app.get("/message/:id", (request, response) => {
-    const {id} = request.params
-    response.send(`O id é de: ${id}`)
-})
-
-app.get("/user", (request, response) => {
-    const {page, limit} = request.query
-    response.send(`A página é: ${page}.\n O limite atual é de: ${limit}`)
-})
 
 app.listen(PORT, () => {console.log(`Server is running on Port: ${PORT}`)})
