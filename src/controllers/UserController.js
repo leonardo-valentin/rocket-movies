615dca9f6f444fab6f810b9bbbd15e0f4d086721
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-21 17:29:34 -0300

    adding CONTROLLERS

---
+++ src/controllers/UserController.js
@@ -0,0 +1,9 @@
+class UserController {
+    /*
+    * index  - GET para listar vários registros.
+    * show   - GET para mostrar um registro específico.
+    * create - POST para criar um novo registro.
+    * update - PUT para atualizar um registro.
+    * delete - DELETE para remover um registro.
+    */
+}
