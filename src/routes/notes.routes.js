d8e34e0dc4840c77f8162219bcbdc5ca594cc79c
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-29 18:59:06 -0300

    changes today

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,6 +9,8 @@ const NotesController = require("../controllers/NotesController")
 const notesController = new NotesController()
 
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.showgit)
+
 
 
 module.exports = notesRoutes
