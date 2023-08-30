dae35cb8727b4b60ea71f403e614371f3449d988
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-08-30 19:33:36 -0300

    bug fix tags not in index notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -75,22 +75,22 @@ class NotesController {
             .innerJoin("notes", "notes.id", "tags.note_id")
             .orderBy("notes.title")
 
-            const userTags = await knex("tags").where({user_id})
-
-            const notesWithTags = notes.map(note => {
-                const noteTags = userTags.filter(tag => tag.note_id === note.id)
-                return {
-                  ...note,
-                  tags: noteTags, 
-                } 
-            })
-            return response.json(notesWithTags)
         } else {
 
             notes = await knex("notes").where({user_id}).whereLike("title", `%${title}%`).orderBy("title")
     
-            return response.json(notes)
         }
+
+        const userTags = await knex("tags").where({user_id})
+
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id)
+            return {
+              ...note,
+              tags: noteTags, 
+            } 
+        })
+        return response.json(notesWithTags)
         
     }
 }
